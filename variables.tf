3c1c375283593e1311e23d8a266bb44f1f55da3c
Nirh237 <nirh237@gmail.com>
2021-04-17 23:08:58 +0300

    update files

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "admin_username" {
+description = "admin_username"
+type = string
+default = "nirh237"
+}
+
+variable "admin_password" {
+description = "admin_password"
+type = string
+default = "Azure888801!"
+}
